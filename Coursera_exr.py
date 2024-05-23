6c43b01be3acaff4a6b1d4a2d4e973eea3975bc3
Yrysbeav <mzamirbekovic02@gmail.com>
2024-05-23 07:32:27 -0500

    Solved Close #2

--- Coursera_exr.py
+++ Coursera_exr.py
@@ -4,12 +4,12 @@ def is_power_of_two(number):
   # without leaving a remainder. How can you change the while loop to
   # avoid a Python ZeroDivisionError?
   while number % 2 == 0:
-    
+
     number = number / 2
+    if number < 2:
+      break
     
-    
-  # If after dividing by 2 "number" equals 1, then "number" is a power
-  # of 2.
+  # If after dividing by 2 "number" equals 1, then "number" is a power of 2.
   if number == 1:
     return True
   return False
